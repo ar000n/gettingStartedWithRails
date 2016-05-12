802483f7e42272ff663f54ee1eabb5aede634e0d
Arun <arun@in-arunsathin.local>
2016-05-12 15:16:13 +0530

    6 should need to start the game and should end with exactly 16

--- play/app/controllers/welcome_controller.rb
+++ play/app/controllers/welcome_controller.rb
@@ -1,15 +1,44 @@
 class WelcomeController < ApplicationController
-  @@currentPosition = 0
 
   def index
     if request.method.eql? 'GET'
       @diceValue = ""
-      @@currentPosition = 0
+      @@currentPosition = -5
+      @@gameStart = false
     else
       @diceValue = 1 + rand(6)
-      @@currentPosition += @diceValue
-      @currentPosition = @@currentPosition
+      triggerGame @diceValue
+      if @@gameStart
+        updateCurrentPosition
+      else
+        @gameStatus = "Need a 6 to start the journey...!!"
+      end
     end
+  end
 
+  private
+  def updateCurrentPosition
+    @@currentPosition += @diceValue
+    check = @@currentPosition<=>16
+    if check == -1
+      @currentPosition = @@currentPosition
+    else
+      if check ==0
+        @currentPosition = @@currentPosition
+        @gameStatus = 'you won......!!'
+      else
+        if check ==1
+          @@currentPosition-=@diceValue
+          @currentPosition = @@currentPosition
+        end
+      end
+    end
+  end
+
+  
+  def triggerGame number
+    if number == 6
+      @@gameStart = true
+    end
   end
 end
