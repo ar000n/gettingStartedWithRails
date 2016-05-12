95aea38e3b55a9445a06af1d99f83e145d76ab2a
Arun <arun@in-arunsathin.local>
2016-05-12 12:40:55 +0530

    coins started moving

--- play/app/controllers/welcome_controller.rb
+++ play/app/controllers/welcome_controller.rb
@@ -1,11 +1,15 @@
 class WelcomeController < ApplicationController
+  @@currentPosition = 0
 
   def index
-    @diceValue = 0
-  end
-  def new
-    # puts "----------------------------------------------"+params.to_s
-    # @diceValue = 12
-    redirect_to :action => 'index'
+    if request.method.eql? 'GET'
+      @diceValue = ""
+      @@currentPosition = 0
+    else
+      @diceValue = 1 + rand(6)
+      @@currentPosition += @diceValue
+      @currentPosition = @@currentPosition
+    end
+
   end
 end
